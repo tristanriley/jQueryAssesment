b69d3a96fe27fa48dc46565355fa2be47c675360
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-20 12:15:59 -0700

    test done

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<script type="text/javascript" src="main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</head>
+<body>
+	<div class="dan">
+		<p>
+			Danny Boy
+		</p>
+	</div>
+	<div id="iwao">
+		<p>
+			The Pipes are calling
+		</p>
+	</div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.dan').click(function{
+
+		alert("Hey you clicked on me")
+
+	});
+
+
+
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function{
+
+		alert("Hey you clicked on me again")
+
+	});
+
+
+
+});
